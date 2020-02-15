17a5d501f6637206fe95fca7717193699a2ba90c
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 18:47:51 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,11 +2,8 @@ require 'pry'
 collection = (1..100).to_a
 
 def my_find(collection)
-    
-
  i = 0
   while i < collection.length
- 
    return collection[i] if yield(collection[i])
     i = i + 1
   end
