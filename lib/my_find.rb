c05c16421aa6f263e92de010460ac23943142844
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 18:50:46 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,17 @@
 require 'pry'
+collection = (1..100).to_a
 
 def my_find(collection)
-
+ i = 0
+  while i < collection.length
+   return collection[i] if yield(collection[i])
+   binding.pry
+    i = i + 1
+  end
 end
+
+#this code returns collection[i] if the i is divisible by 3 and 5, i yielding to the block when the 
+#function is called
+
+
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
